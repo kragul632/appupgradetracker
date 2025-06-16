250ffb482638ad86a7444f61579e5c6e320a804c
2387977 <2387977@LTIN533736>
2025-06-16 10:23:49 +0530

    ui enhancements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { EditComponent } from './edit/edit.component';
 import { ViewdataComponent } from './viewdata/viewdata.component';
 import { UploadComponent } from './upload/upload.component';
 import { AdddataComponent } from './adddata/adddata.component';
+import { AddEffortComponent } from './components/add-effort/add-effort.component';
 
 const routes: Routes = [
     { path: 'upload', component: UploadComponent },
     { path: 'viewdata', component: ViewdataComponent },
     { path: 'edit', component: EditComponent },
     { path: 'add', component: AdddataComponent }, 
+    {path: 'add-effort', component: AddEffortComponent},
     { path: '', redirectTo: '/viewdata', pathMatch: 'full' }
   ];
   
